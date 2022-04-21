7f5b4f7f9d7d87b70c3c71d30c37340f9c277b8e
Rodrigo Moraes <rodrigobatsmoraes@hotmail.com>
2022-04-21 19:58:25 -0300

    don't enable opengl backend on wasm32 target

--- src/backends.rs
+++ src/backends.rs
@@ -3,7 +3,7 @@ mod webgl;
 #[cfg(all(feature = "webgl", target_arch = "wasm32"))]
 pub use webgl::WebGLSpriteRender;
 
-#[cfg(feature = "opengl")]
+#[cfg(all(not(target_arch = "wasm32"), feature = "opengl"))]
 mod opengl;
-#[cfg(feature = "opengl")]
+#[cfg(all(not(target_arch = "wasm32"), feature = "opengl"))]
 pub use opengl::GLSpriteRender;
